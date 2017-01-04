d9fabd954fa7118472ebd59f96ffd4bff1e21e01
vibincodetok <vibinc00@gmail.com>
2017-01-04 16:20:25 +0530

    pages and components seperated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,16 @@ import { RouterModule, Routes } from '@angular/router';
   
 //components
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { HomeComponent } from './home/home.component';
-import { LeaderBoxComponent } from './leader-box/leader-box.component';
-import { StreamBoxComponent } from './stream-box/stream-box.component';
-import { OutBoxComponent } from './out-box/out-box.component';
-import { LoginComponent } from './login/login.component';
-
-
+import { HomeComponent } from './pages/home/home.component';
+import { LeaderBoxComponent } from './components/leader-box/leader-box.component';
+import { StreamBoxComponent } from './components/stream-box/stream-box.component';
+import { OutBoxComponent } from './components/out-box/out-box.component';
+import { LoginComponent } from './components/login/login.component';
 
 //providers goes here
 import {PeerService} from './providers/peer.service';
 
-=======
-import { HomeComponent } from './pages/home/home.component';
-import { LeaderBoxComponent } from './pages/leader-box/leader-box.component';
-import { StreamBoxComponent } from './pages/stream-box/stream-box.component';
-import { OutBoxComponent } from './pages/out-box/out-box.component';
-import { LoginComponent } from './pages/login/login.component';
 
-
->>>>>>> d8a0af8826406a7922fe9b0acf861744389ad57b
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
