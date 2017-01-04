d8a0af8826406a7922fe9b0acf861744389ad57b
vibincodetok <vibinc00@gmail.com>
2017-01-04 12:57:19 +0530

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
   
-
+//components
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LeaderBoxComponent } from './leader-box/leader-box.component';
-import { StreamBoxComponent } from './stream-box/stream-box.component';
-import { OutBoxComponent } from './out-box/out-box.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LeaderBoxComponent } from './pages/leader-box/leader-box.component';
+import { StreamBoxComponent } from './pages/stream-box/stream-box.component';
+import { OutBoxComponent } from './pages/out-box/out-box.component';
+import { LoginComponent } from './pages/login/login.component';
+
+
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
